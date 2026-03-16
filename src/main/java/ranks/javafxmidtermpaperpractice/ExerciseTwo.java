f5df81628fb466541c1eb3364844110a46b303ef
Tebello Rankuatsana <justranku@gmail.com>
2026-03-16 20:24:45 +0200

    first exercise

--- src/main/java/ranks/javafxmidtermpaperpractice/ExerciseTwo.java
+++ src/main/java/ranks/javafxmidtermpaperpractice/ExerciseTwo.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ExerciseTwo {
 
     @FXML
@@ -15,7 +18,14 @@ public class ExerciseTwo {
 
     @FXML
     void handleWrite(ActionEvent event) {
-
+        try{
+            FileWriter fw = new FileWriter("exerciseTwo.txt",true);
+            String line;
+            fw.write(txtMesaage.getText()+"\n");
+            fw.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
