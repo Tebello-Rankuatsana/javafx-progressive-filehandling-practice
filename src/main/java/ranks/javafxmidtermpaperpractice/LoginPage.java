bbbfc803d097a11d4bbeb86cdc40fb73c5d86be5
Tebello Rankuatsana <justranku@gmail.com>
2026-03-19 02:38:10 +0200

    the logic for the login page

---
+++ src/main/java/ranks/javafxmidtermpaperpractice/LoginPage.java
@@ -0,0 +1,33 @@
+package ranks.javafxmidtermpaperpractice;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginPage {
+
+    @FXML
+    private Label lblEmail;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private PasswordField psdPass;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRegister(ActionEvent event) {
+
+    }
+
+}
