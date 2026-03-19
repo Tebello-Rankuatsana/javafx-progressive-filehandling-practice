e2375efe044b274cf3dd425164404848095c7677
Tebello Rankuatsana <justranku@gmail.com>
2026-03-19 02:24:50 +0200

    remembering file handling ver 2

--- src/main/java/ranks/javafxmidtermpaperpractice/RememberingFileHandling.java
+++ src/main/java/ranks/javafxmidtermpaperpractice/RememberingFileHandling.java
@@ -2,11 +2,7 @@ package ranks.javafxmidtermpaperpractice;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 
 import java.io.*;
 
@@ -38,34 +34,71 @@ public class RememberingFileHandling {
 
     @FXML
     void handleLoad(ActionEvent event) {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("remembering.txt"));
+//        try {
+//            BufferedReader br = new BufferedReader(new FileReader("remembering.txt"));
+//            String line;
+//            txtDisplay.clear();// making it so that it doesn't repeat stuff
+//            while((line = br.readLine()) != null){
+//                txtDisplay.appendText(line+"\n");
+//            }
+//            br.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("remembering_two.txt"));
             String line;
-            txtDisplay.clear();// making it so that it doesn't repeat stuff
-            while((line = br.readLine()) != null){
-                txtDisplay.appendText(line+"\n");
-            }
-            br.close();
-        } catch (IOException e) {
+            txtDisplay.clear();
+            while((line = br.readLine()) != null)
+                txtDisplay.appendText(line + "\n");
+        }catch(IOException e){
             e.printStackTrace();
         }
     }
 
     @FXML
     void handleWrite(ActionEvent event) {
+//        try {
+//            BufferedWriter bw = new BufferedWriter(new FileWriter("remembering.txt",true));
+//            bw.write("Name: "+txtName.getText()+"\n");
+//            bw.write("Surname: "+txtSurname.getText()+"\n");
+//            if(rbMale.isSelected())
+//                bw.write("Gender: Male"+"\n");
+//            if(rbFemale.isSelected())
+//                bw.write("Gender: Female"+"\n");
+//            bw.write("____________________________"+"\n");
+//            bw.close();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
         try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter("remembering.txt",true));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("remembering_two.txt",true));
             bw.write("Name: "+txtName.getText()+"\n");
             bw.write("Surname: "+txtSurname.getText()+"\n");
             if(rbMale.isSelected())
                 bw.write("Gender: Male"+"\n");
             if(rbFemale.isSelected())
                 bw.write("Gender: Female"+"\n");
-            bw.write("____________________________"+"\n");
+
+            bw.write("_______________________"+"\n");
+            if(txtSurname.getText().isEmpty() || txtName.getText().isEmpty()){
+                Alert warning = new Alert(Alert.AlertType.ERROR);
+                warning.setTitle("Fill in Credentials");
+                warning.setContentText("Fill in proper information");
+                warning.showAndWait();
+            }else {
+//            adding an alert(cause I'm HIM)
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("File Operation");
+                alert.setHeaderText(null);
+                alert.setContentText("Information was written successfully!!!");
+                alert.showAndWait();
+            }
             bw.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 
 }
